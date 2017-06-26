68f8387e608191b44ef02265e464f4d5b43cb62e
midhun.lingam <mlingam@asu.edu>
2017-06-25 22:21:28 -0700

    Added 2 tabs. Should add the map functionality in the 2nd tab.

--- src/main/java/info/androidhive/recyclerview/MainActivity.java
+++ src/main/java/info/androidhive/recyclerview/MainActivity.java
@@ -4,46 +4,58 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    ;
-    private RecyclerView recyclerView;
-    private PlaceAdapter mAdapter;
+
     public static String imageDirPath;
     public static File directory;
+    public static PlaceAdapter mAdapter;
 
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
         directory = cw.getDir("imageDir", Context.MODE_APPEND);
         imageDirPath = directory.getAbsolutePath();
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
 
 
         //recyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        mAdapter = new PlaceAdapter(getBaseContext(),(LinearLayoutManager)mLayoutManager);
-        recyclerView.setAdapter(mAdapter);
     }
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -65,6 +77,84 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == 1) {
             Log.v("name1", data.getStringExtra("name"));
             mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private RecyclerView recyclerView;
+        View rootView;
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            rootView = inflater.inflate(R.layout.fragment_main, container, false);
+//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+            recyclerView = (RecyclerView)rootView.findViewById(R.id.recycler_view);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(rootView.getContext());
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.addItemDecoration(new DividerItemDecoration(rootView.getContext(), LinearLayoutManager.VERTICAL));
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            mAdapter = new PlaceAdapter(rootView.getContext(),(LinearLayoutManager)mLayoutManager);
+            recyclerView.setAdapter(mAdapter);
+            return rootView;
+        }
+
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+            }
+            return null;
         }
     }
 }
--- src/main/java/info/androidhive/recyclerview/OnDatabaseChangedListener.java
+++ src/main/java/info/androidhive/recyclerview/OnDatabaseChangedListener.java
@@ -1,9 +1,6 @@
 package info.androidhive.recyclerview;
 
-/**
- * Created by Daniel on 1/3/2015.
- * Listen for add/rename items in database
- */
+
 public interface OnDatabaseChangedListener{
     void onNewDatabaseEntryAdded();
     void onDatabaseEntryRenamed();
